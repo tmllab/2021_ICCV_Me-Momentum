22801f76836dc7aa03d5ced9f1004e8f3bd19eb1
USYD-TMLLAB <75194327+tmllab@users.noreply.github.com>
2022-02-24 22:03:09 +1100

    Update ClothingData.py
    Remove the unnecessary file

--- ClothingData.py
+++ ClothingData.py
@@ -13,24 +13,6 @@ def read_key_list(file_name):
     return np.array(keyList)
 
 
-def read_share_list(file_name):
-    key_list = []
-    noisy_labels_list = []
-    clean_labels_list = []
-    fo = open(file_name, "r")
-    print("Read file:", fo.name)
-    for line in fo.readlines():
-        strs = line.strip().split(";;")
-        if(len(strs) == 3):
-            key_list.append(strs[0])
-            noisy_labels_list.append(strs[1])
-            clean_labels_list.append(strs[2])
-        else:
-            print(line)
-    fo.close()
-    return np.array(key_list), np.array(noisy_labels_list, dtype=np.int), np.array(clean_labels_list, dtype=np.int)
-
-
 def read_kv_map(file_name):
     kv_map = {}
     fo = open(file_name, "r")
@@ -63,8 +45,6 @@ clean_val_key_file = 'data/Clothing1M_Official/annotations/clean_val_key_list.tx
 noisy_train_key_file = 'data/Clothing1M_Official/annotations/noisy_train_key_list.txt'
 noisy_label_kv = 'data/Clothing1M_Official/annotations/noisy_label_kv.txt'
 clean_label_kv = 'data/Clothing1M_Official/annotations/clean_label_kv.txt'
-share_file = 'data/Clothing1M_Official/annotations/share.txt'
-
 
 noisy_train_key_list = read_key_list(noisy_train_key_file)
 clean_val_key_list = read_key_list(clean_val_key_file)
@@ -84,9 +64,6 @@ clean_test_labels = get_value_from_kv(clean_test_key_list, clean_label_kv_map)
 
 print(noisy_labels.shape, clean_val_labels.shape, clean_test_labels.shape)
 
-share_key_list, share_noisy_labels_list, share_clean_labels_list = read_share_list(share_file)
-print(share_key_list.shape, share_noisy_labels_list.shape, share_clean_labels_list.shape)
-
 kvDic = {}
 kvDic['train_data'] = noisy_train_key_list
 kvDic['train_labels'] = noisy_labels
@@ -94,7 +71,4 @@ kvDic['clean_val_data'] = clean_val_key_list
 kvDic['clean_val_labels'] = clean_val_labels
 kvDic['test_data'] = clean_test_key_list
 kvDic['test_labels'] = clean_test_labels
-kvDic['share_data'] = share_key_list
-kvDic['share_noisy_labels'] = share_noisy_labels_list
-kvDic['share_clean_labels'] = share_clean_labels_list
 np.save(save_file, kvDic, allow_pickle=True)
